b7e4e1e8f530b4d9e559e23ed78a0926a9e3d8a8
unknown <wholeomarfaruk@gmail.com>
2024-02-28 02:34:35 +0600

    sidebar update

--- index.php
+++ index.php
@@ -44,27 +44,47 @@
                 <div class="sidebar">
                     <div class="inner-header">
                         <div class="container">
-                            <div class="row">
-                            <div class="col-6">
-                                    <div class="close-btn"><a href="javascript:void(0)"><i class="fa fa-arrow-left" aria-hidden="true"></i>Back to Home</a></div>
-                                </div>
-                                <div class="col-6"></div>
-
-                            </div>
+                        <div class="row">
+                                        <div class="col-auto">
+                                        <div class="close-btn"><a href="javascript:void(0)"><i class="fa fa-arrow-left" aria-hidden="true"></i></a></div>
+                                        </div>
+                                        <div class="col">
+                                            <h5 class="text-center text-white">Menu</h5>
+                                        </div>
+                                    </div>
                         </div>
                     </div>
                     <div class="inner-body">
                         <div class="top-bar">
                             <div class="profile">
-                                <div class="pic"><img src="assets/img/profile-pic.png" alt=""></div>
-                                <div class="name">Rafi Telecom</div>
+                                <div class="owner-info row align-items-center py-2">
+                                    <div class="col-auto">
+                                        <div class="pic"><img src="assets/img/profile-pic.png" alt=""></div>
+                                    </div>
+                                    <div class="col-auto">
+
+                                        <div class="name">Rafi Telecom</div>
+                                        <div class="phone">+88 01684285963</div>
+                                        <div class="owner-name">Owner Name: Md. Omar Faruk</div>
+
+                                    </div>
+
+                                </div>
+                                <div class="dso-info row">
+                                    <div class="col-auto">
+                                        <div class="dso-name"> DSO Name: Md. Omar Faruk</div>
+                                        <div class="dso-phone">DSO Phone: <a href="tel:+8801684285963"><i class="fa fa-phone" aria-hidden="true"></i> +8801684285963</a></div>
+                                    </div>
+                                </div>
+
                             </div>
-                            <div class="alert-area">
+                            <!-- <div class="alert-area">
                                 <div class="notification">9+</div>
-                            </div>
+                            </div> -->
                         </div>
                         <div class="menu-bar">
-                            <nav id="main_menu">
+                            <div></div>
+                            <nav class="sidebar-menu">
                                 <ul>
                                     <li><a href="#"><span class="icon"><i class="fa fa-tachometer" aria-hidden="true"></i></span>Dashboard</a></li>
                                     <li><a href="#"><span class="icon"><i class="fa fa-money" aria-hidden="true"></i></span>Cashout</a></li>
@@ -74,7 +94,11 @@
                             </nav>
                         </div>
                     </div>
-                    <div class="inner-footer"></div>
+                    <div class="inner-footer">
+                        <div class="container">
+                            <span class="title">Online Support:</span> <a href="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a><a href="tel:+8801684285963"><i class="fa fa-phone" aria-hidden="true"></i> +88 01684285963</a>
+                        </div>
+                    </div>
 
                 </div>
             </div>
